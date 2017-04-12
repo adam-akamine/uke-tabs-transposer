3fbf7de558349651e316960e983e864181bc337b
kekuso <kekuso808@gmail.com>
2017-04-11 21:58:57 -1000

    add objects for low and mid octaves

--- transposer.js
+++ transposer.js
@@ -1,7 +1,101 @@
+'use strict';
+
 const song = require('./mia_and_sebastian_theme');
 
 const songArr = song.MIA_AND_SEBASTIAN_THEME.split('');
+const notesLow = {
+  C: {
+    stringLine: 3,
+    fret: 0,
+  },
+  Db: {
+    stringLine: 3,
+    fret: 1,
+  },
+  D: {
+    stringLine: 3,
+    fret: 2,
+  },
+  E: {
+    stringLine: 2,
+    fret: 0,
+  },
+  F: {
+    stringLine: 2,
+    fret: 1,
+  },
+  Gb: {
+    stringLine: 2,
+    fret: 2,
+  },
+  G: {
+    stringLine: 2,
+    fret: 3,
+  },
+  Ab: {
+    stringLine: 1,
+    fret: 1,
+  },
+  A: {
+    stringLine: 1,
+    fret: 0,
+  },
+  Bb: {
+    stringLine: 1,
+    fret: 1,
+  },
+  B: {
+    stringLine: 1,
+    fret: 2,
+  }
+};
 
+const notesMid = {
+  C: {
+    stringLine: 3,
+    fret: 3,
+  },
+  Db: {
+    stringLine: 3,
+    fret: 4,
+  },
+  D: {
+    stringLine: 3,
+    fret: 5,
+  },
+  E: {
+    stringLine: 3,
+    fret: 6,
+  },
+  F: {
+    stringLine: 3,
+    fret: 7,
+  },
+  Gb: {
+    stringLine: 3,
+    fret: 8,
+  },
+  G: {
+    stringLine: 3,
+    fret: 9,
+  },
+  Ab: {
+    stringLine: 3,
+    fret: 10,
+  },
+  A: {
+    stringLine: 3,
+    fret: 11,
+  },
+  Bb: {
+    stringLine: 3,
+    fret: 12,
+  },
+  B: {
+    stringLine: 3,
+    fret: 13,
+  }
+}
 function getChord(tab) {
 
 }
